d4a4ee9809a5c91bd831bd5bf1df2f1762f4fa7e
lirael <wazzroboy@gmail.com>
2020-05-07 12:42:54 +0400

    fix(migration): add password to init user

--- backend/src/migrations/init.ts
+++ backend/src/migrations/init.ts
@@ -12,6 +12,7 @@ export class InitialData1564132613702 implements MigrationInterface {
     admin.phone = '+79000000000';
     admin.birthDate = new Date(1948, 1, 1);
     admin.startWorkDate = new Date(2012, 12, 21);
+    (admin as any).password = '123123';
 
     await admin.save();
   }
